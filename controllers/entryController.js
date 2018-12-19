82536333887e82c721ac7de45b848890e870264d
sahenke13 <shenke13@gmail.com>
2018-12-19 12:58:07 -0600

    added Controllers for Models

---
+++ controllers/entryController.js
@@ -0,0 +1,47 @@
+const db = require('../models');
+
+
+module.exports = {
+    create: function(req, res) {
+        db.Entry
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    findByUser: function(req, res){
+        db.Entry
+            .find({userId: req.params.userId})
+            .sort({type: 1, name: 1})
+            .then(dbModel => {
+                res.json(dbModel);
+            })
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.Entry
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    },
+    update: function(req, res){
+        db.Entry
+            .findOneAndUpdate({_id: req.params.id}, req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res){
+        db.Entry
+            .findById({_id: req.params.id})
+            .then(dbModel => dbModel.remove())
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    }
+    };
+
+
+
+
+
+
+
+}
---
+++ models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Entry: require('./Entry'),
+    Story: require('./Story'),
+    User: require('./User')
+};
