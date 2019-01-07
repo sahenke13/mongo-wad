82ba59dda4565e3ee68473d8bc3bcb5cc7cfb4b5
Jack Morrissey <j.morrissey.16@gmail.com>
2019-01-07 13:41:37 -0600

    building the create story form

--- client/src/pages/CreateStory.js
+++ client/src/pages/CreateStory.js
@@ -18,8 +18,30 @@ export default class CreateStory extends Component {
 
 render() {
     return (
-      <div>
-        
+      <div className ="container">
+        <div className = "row">
+            <div className = "col-md-12">
+                
+                <form>
+                {/* Title Entry */}
+                    <div className = "form-group">
+                        <label for="title">Title</label>
+                        <input type ="text" className ="form-control" id = "storyTitle" placeholder = "Give your Story a name"/>
+                    </div>
+                {/* Genre Entry */}
+                    <div className = "form-group">
+                        <label for="title">Genre</label>
+                        <input type ="text" className ="form-control" id = "genre" placeholder = "Decide on the genre"/>
+                    </div>
+                {/* Root Entry */}
+                    <div className = "form-group">
+                        <label for="title">Root Entry</label>
+                        <textarea className ="form-control" id = "rootEntry" rows="10" placeholder = "Start your story here"/>
+                    </div>
+                </form>
+                
+            </div>
+        </div>
       </div>
     )
   }
