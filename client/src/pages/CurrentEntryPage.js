bfe1d51359c7a340b2192c8567e493dac6b016f3
sahenke13 <shenke13@gmail.com>
2019-01-08 19:19:53 -0600

    Added Modal to Displayed Entry

--- client/src/pages/CurrentEntryPage.js
+++ client/src/pages/CurrentEntryPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import DisplayEntry from "../components/DisplayedEntry";
+import Entry from "../components/Entry";
+import NavBar from "../components/NavBar";
 
 export default class CurrentEntryPage extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <DisplayEntry />
       </div>
     );
