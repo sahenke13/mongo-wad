e811312bd15322bafdbd287b540af0b32a58341e
sahenke13 <shenke13@gmail.com>
2019-01-23 09:07:04 -0600

    A little styling on the Create a Story

--- routes/api/entry.js
+++ routes/api/entry.js
@@ -7,13 +7,7 @@ router
   .post(entryController.create);
 
 router.route("/root/:storyId").get(entryController.findRootEntry);
-<<<<<<< HEAD
 
-router.route("/next/:nextEntriesArray").get(entryController.findNextEntries);
-
-=======
-  
->>>>>>> aeb471b563095ce8905957387e08f5a0814ad027
 router
   .route("/:id")
   .put(entryController.update)
