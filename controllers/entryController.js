0193ae0e8a70641c8266d8c5310eaf35b287d2de
Jack Morrissey <j.morrissey.16@gmail.com>
2019-01-22 19:36:31 -0600

    header

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -1,83 +1,6 @@
 const db = require("../models");
 
 module.exports = {
-<<<<<<< HEAD
-    create: function(req, res) {
-        db.Entry
-            .create(req.body)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    },
-    findAll: function(req, res) {
-        db.Entry
-            .find()
-            .sort({ type: 1, name: 1})
-            .then(dbModel => {
-                res.json(dbModel);
-            })
-            .catch(err => res.status(422).json(err));
-    },
-    findByUser: function(req, res){
-        db.Entry
-            .find({userId: req.params.userId})
-            .sort({type: 1, name: 1})
-            .then(dbModel => {
-                res.json(dbModel);
-            })
-            .catch(err => res.status(422).json(err));
-    },
-    findRootEntry: function(req, res) {
-        db.Entry
-            .find({storyId: req.params.storyId})
-            .then(dbModel => {
-                res.json(dbModel);
-            })
-            .catch(err => res.status(422).json(err))
-    },
-    findNextEntries: function(req, res) {
-        let entriesArray = req.params.nextEntriesArray.split(',');
-            db.Entry
-                .find({_id: { $in: entriesArray}})
-                .then(dbModel => {
-                    res.json(dbModel);
-                })
-                .catch(err => res.status(422).json(err))
-            
-    },
-    findById: function(req, res) {
-        db.Entry
-            .findById(req.params.id)
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err))
-    },
-    update: function(req, res){
-        db.Entry
-            .updateOne(
-                {_id: req.params.id}, 
-                { $push: {nextEntryArray: req.body.idToPush}}, 
-                function(err, raw) {
-                if (err) return handleError(err);
-                console.log("The raw response from Mongo was", raw)
-                console.log(req.body)
-            })
-            
-    },
-    remove: function(req, res){
-        db.Entry
-            .findById({_id: req.params.id})
-            .then(dbModel => dbModel.remove())
-            .then(dbModel => res.json(dbModel))
-            .catch(err => res.status(422).json(err));
-    }
-};
-
-
-
-
-
-
-
-=======
   create: function(req, res) {
     db.Entry.create(req.body)
       .then(dbModel => res.json(dbModel))
@@ -135,4 +58,3 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   }
 };
->>>>>>> 163b2c1a0cf85105338587f5689a0de9973c605e
