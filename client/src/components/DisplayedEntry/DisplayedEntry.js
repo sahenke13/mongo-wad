4adc70d8fd6a4e6a9ead66b6fb4c2253454220a8
sahenke13 <shenke13@gmail.com>
2019-02-21 09:51:02 -0600

    fixed nextEntries when newEntry created

--- client/src/components/DisplayedEntry/DisplayedEntry.js
+++ client/src/components/DisplayedEntry/DisplayedEntry.js
@@ -9,225 +9,226 @@ import CurrentEntry from "../CurrentEntry";
 import NextEntryArray from "../NextEntryArray";
 
 export default class DisplayedEntry extends Component {
-	state = {
-		yourStory: [],
-		storyInfo: [],
-		currentEntry: "",
-		firstEntriesArray: [],
-		previousEntryId: null,
-		currentId: "",
-		newEntryContent: "",
-		nextEntriesArray: []
-	};
+  state = {
+    yourStory: [],
+    storyInfo: [],
+    currentEntry: "",
+    firstEntriesArray: [],
+    previousEntryId: null,
+    currentId: "",
+    newEntryContent: "",
+    nextEntriesArray: []
+  };
 
-	componentDidMount = () => {
-		this.handleFindStory(this.props.id);
-	};
+  componentDidMount = () => {
+    this.handleFindStory(this.props.id);
+  };
 
-	handleInputChange = event => {
-		const { name, value } = event.target;
-		this.setState({
-			[name]: value
-		});
-	};
-	// maybe do something like this but instead of passing id thru pass thru nextEntryArray in params??
-	handleFindStory = id => {
-		API.getStory(id)
-			.then(res => {
-				this.setState(() => {
-					return { storyInfo: res.data };
-				});
-			})
-			.catch(err => console.log("this is an error", err));
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+  // maybe do something like this but instead of passing id thru pass thru nextEntryArray in params??
+  handleFindStory = id => {
+    API.getStory(id)
+      .then(res => {
+        this.setState(() => {
+          return { storyInfo: res.data };
+        });
+      })
+      .catch(err => console.log("this is an error", err));
 
-		API.displayRootEntries(id).then(res => {
-			this.setState({
-				firstEntriesArray: res.data
-			});
-		});
-	};
+    API.displayRootEntries(id).then(res => {
+      this.setState({
+        firstEntriesArray: res.data
+      });
+    });
+  };
 
-	//need to set voteCount to 0?? grab votes??
-	handleNewEntrySubmit = () => {
-		let EntryId = this.state.currentId ? this.state.currentId : null;
-		API.saveEntry({
-			storyId: this.state.storyInfo._id,
-			content: this.state.newEntryContent,
-			previousEntryId: EntryId,
-			voteCount: 0
-		})
-			.then(res => {
-				let prevId = res.data.previousEntryId;
-				let curEntry = res.data;
-				let curId = res.data._id;
+  //need to set voteCount to 0?? grab votes??
+  handleNewEntrySubmit = () => {
+    let EntryId = this.state.currentId ? this.state.currentId : null;
+    API.saveEntry({
+      storyId: this.state.storyInfo._id,
+      content: this.state.newEntryContent,
+      previousEntryId: EntryId,
+      voteCount: 0
+    })
+      .then(res => {
+        let prevId = res.data.previousEntryId;
+        let curEntry = res.data;
+        let curId = res.data._id;
 
-				API.updateEntry(prevId, {
-					$push: {
-						nextEntryArray: curId
-					}
-				});
+        API.updateEntry(prevId, {
+          $push: {
+            nextEntryArray: curId
+          }
+        });
 
-				let yourStoryArray = [...this.state.yourStory, curEntry];
+        let yourStoryArray = [...this.state.yourStory, curEntry];
 
-				this.setState({
-					currentEntry: curEntry,
-					previousEntryId: prevId,
-					currentId: curId,
-					yourStory: yourStoryArray,
-					newEntryContent: ""
-				});
-			})
-			.catch(err => console.log("this is an error", err));
-	};
+        this.setState({
+          currentEntry: curEntry,
+          previousEntryId: prevId,
+          currentId: curId,
+          yourStory: yourStoryArray,
+          newEntryContent: "",
+          nextEntriesArray: []
+        });
+      })
+      .catch(err => console.log("this is an error", err));
+  };
 
-	handleEntryClicked = id => {
-		this.setState({ currentId: id, nextEntriesArray: [] }, () => {
-			this.handleUpdateCurrentEntry(id);
-		});
-	};
+  handleEntryClicked = id => {
+    this.setState({ currentId: id, nextEntriesArray: [] }, () => {
+      this.handleUpdateCurrentEntry(id);
+    });
+  };
 
-	displayNextEntries = nextEntryArray => {
-		API.displayNextEntries(nextEntryArray).then(res => {
-			let nextEntries = res.data;
+  displayNextEntries = nextEntryArray => {
+    API.displayNextEntries(nextEntryArray).then(res => {
+      let nextEntries = res.data;
 
-			this.setState({
-				nextEntriesArray: nextEntries
-			});
-		});
-	};
+      this.setState({
+        nextEntriesArray: nextEntries
+      });
+    });
+  };
 
-	handleUpdateCurrentEntry = id => {
-		API.displayEntry(id)
-			.then(res => {
-				let selectedSegment = res.data;
-				let yourStoryArray = [...this.state.yourStory, selectedSegment];
-				this.setState(() => {
-					return {
-						currentEntry: res.data,
-						previousEntryId: res.data.previousEntryId,
-						yourStory: yourStoryArray
-					};
-				});
-				this.displayNextEntries(res.data.nextEntryArray);
-			})
-			.catch(err => console.log("this is an error", err));
-	};
+  handleUpdateCurrentEntry = id => {
+    API.displayEntry(id)
+      .then(res => {
+        let selectedSegment = res.data;
+        let yourStoryArray = [...this.state.yourStory, selectedSegment];
+        this.setState(() => {
+          return {
+            currentEntry: res.data,
+            previousEntryId: res.data.previousEntryId,
+            yourStory: yourStoryArray
+          };
+        });
+        this.displayNextEntries(res.data.nextEntryArray);
+      })
+      .catch(err => console.log("this is an error", err));
+  };
 
-	handleBackButtonUpdateCurrentEntry = id => {
-		let yourStoryArray = [...this.state.yourStory];
-		API.displayEntry(id).then(res => {
-			let item = res.data;
-			this.setState({
-				currentEntry: item,
-				previousEntryId: item.previousEntryId,
-				yourStory: yourStoryArray
-			});
-			this.displayNextEntries(res.data.nextEntryArray);
-		});
-	};
+  handleBackButtonUpdateCurrentEntry = id => {
+    let yourStoryArray = [...this.state.yourStory];
+    API.displayEntry(id).then(res => {
+      let item = res.data;
+      this.setState({
+        currentEntry: item,
+        previousEntryId: item.previousEntryId,
+        yourStory: yourStoryArray
+      });
+      this.displayNextEntries(res.data.nextEntryArray);
+    });
+  };
 
-	handleBackButtonClicked = () => {
-		let yourStoryArray = this.state.yourStory;
-		yourStoryArray.pop();
+  handleBackButtonClicked = () => {
+    let yourStoryArray = this.state.yourStory;
+    yourStoryArray.pop();
 
-		yourStoryArray.length === 0
-			? this.setState(
-					{
-						currentId: "",
-						nextEntriesArray: [],
-						currentEntry: ""
-					},
-					() => {
-						this.handleFindStory(this.props.id);
-					}
-			  )
-			: this.setState(
-					{
-						yourStory: yourStoryArray,
-						currentId: this.state.previousEntryId
-					},
+    yourStoryArray.length === 0
+      ? this.setState(
+          {
+            currentId: "",
+            nextEntriesArray: [],
+            currentEntry: ""
+          },
+          () => {
+            this.handleFindStory(this.props.id);
+          }
+        )
+      : this.setState(
+          {
+            yourStory: yourStoryArray,
+            currentId: this.state.previousEntryId
+          },
 
-					() => {
-						this.handleBackButtonUpdateCurrentEntry(this.state.previousEntryId);
-					}
-			  );
-	};
-	refreshSorting = () => {
-		console.log("handleSorting Clicked");
-		let nextEntryIdArray = this.state.currentEntry.nextEntryArray;
-		API.displayNextEntries(nextEntryIdArray).then(res => {
-			let nextEntries = res.data;
-			this.setState({
-				nextEntriesArray: nextEntries
-			});
-		});
-	};
-	render() {
-		const { title, genre, description } = this.state.storyInfo;
-		const {
-			firstEntriesArray,
-			currentEntry,
-			yourStory,
-			currentId,
-			nextEntriesArray
-		} = this.state;
+          () => {
+            this.handleBackButtonUpdateCurrentEntry(this.state.previousEntryId);
+          }
+        );
+  };
+  refreshSorting = () => {
+    console.log("handleSorting Clicked");
+    let nextEntryIdArray = this.state.currentEntry.nextEntryArray;
+    API.displayNextEntries(nextEntryIdArray).then(res => {
+      let nextEntries = res.data;
+      this.setState({
+        nextEntriesArray: nextEntries
+      });
+    });
+  };
+  render() {
+    const { title, genre, description } = this.state.storyInfo;
+    const {
+      firstEntriesArray,
+      currentEntry,
+      yourStory,
+      currentId,
+      nextEntriesArray
+    } = this.state;
 
-		return (
-			<div className="container" id="displayContainer">
-				{/* <StoryHeader title={title} genre={genre} description={description} /> */}
-				<StoryHeader title={title} genre={genre} description={description} />
-				<div className="row">
-					{this.state.currentId === "" ? (
-						<div className="col-lg-6 m-1">
-							<StartingEntries
-								firstEntriesArray={firstEntriesArray}
-								entryClicked={id => this.handleEntryClicked(id)}
-							/>
-						</div>
-					) : (
-						<div className="col-lg-6 m-2">
-							<CurrentEntry
-								content={currentEntry.content}
-								backButton={this.handleBackButtonClicked}
-							/>
-						</div>
-					)}
-					{/* add your story information below. Probably through a component that
+    return (
+      <div className="container" id="displayContainer">
+        {/* <StoryHeader title={title} genre={genre} description={description} /> */}
+        <StoryHeader title={title} genre={genre} description={description} />
+        <div className="row">
+          {this.state.currentId === "" ? (
+            <div className="col-lg-6 m-1">
+              <StartingEntries
+                firstEntriesArray={firstEntriesArray}
+                entryClicked={id => this.handleEntryClicked(id)}
+              />
+            </div>
+          ) : (
+            <div className="col-lg-6 m-2">
+              <CurrentEntry
+                content={currentEntry.content}
+                backButton={this.handleBackButtonClicked}
+              />
+            </div>
+          )}
+          {/* add your story information below. Probably through a component that
            we pass props to. */}
-					<div className="col-lg-5 m-2">
-						<YourStory stories={yourStory} />
-					</div>
-					<div className="container">
-						{/* does this.state.currentEntry.nextEntryArray exist? */}
+          <div className="col-lg-5 m-2">
+            <YourStory stories={yourStory} />
+          </div>
+          <div className="container">
+            {/* does this.state.currentEntry.nextEntryArray exist? */}
 
-						<NextEntryArray
-							nextEntriesArray={nextEntriesArray}
-							nextEntryClicked={id => this.handleEntryClicked(id)}
-							currentId={currentId}
-							sortEntry={() => this.refreshSorting()}
-						/>
-					</div>
-				</div>
+            <NextEntryArray
+              nextEntriesArray={nextEntriesArray}
+              nextEntryClicked={id => this.handleEntryClicked(id)}
+              currentId={currentId}
+              sortEntry={() => this.refreshSorting()}
+            />
+          </div>
+        </div>
 
-				<div className="row" id="btnGuy">
-					<div className="col-md-12">
-						<button
-							type="button"
-							className="btn text-white submitGuy my-2"
-							data-toggle="modal"
-							data-target="#entryModal"
-						>
-							New Entry
-						</button>
-					</div>
-				</div>
+        <div className="row" id="btnGuy">
+          <div className="col-md-12">
+            <button
+              type="button"
+              className="btn text-white submitGuy my-2"
+              data-toggle="modal"
+              data-target="#entryModal"
+            >
+              New Entry
+            </button>
+          </div>
+        </div>
 
-				<NewEntryModal
-					newEntryContent={this.state.newEntryContent}
-					handleInputChange={this.handleInputChange}
-					newEntrySubmit={this.handleNewEntrySubmit}
-				/>
-			</div>
-		);
-	}
+        <NewEntryModal
+          newEntryContent={this.state.newEntryContent}
+          handleInputChange={this.handleInputChange}
+          newEntrySubmit={this.handleNewEntrySubmit}
+        />
+      </div>
+    );
+  }
 }
