b1a6443cb9f21dad880ec8e076bf1c567db06245
Jack Morrissey <j.morrissey.16@gmail.com>
2019-02-27 23:19:27 -0600

    auth.js & passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,4 +4,24 @@ const LocalStrategy = require("passport-local");
 
 const Users = mongoose.model("Users");
 
-passport.use(new LocalStrategy());
+passport.use(
+	new LocalStrategy(
+		{
+			usernameField: "user[username]",
+			passwordField: "user[password]"
+		},
+		(username, password, done) => {
+			Users.findOne({ username })
+				.then(user => {
+					if (!user || !user.validatePassword(password)) {
+						return done(null, false, {
+							errors: { "username or password": "is invalid" }
+						});
+					}
+
+					return done(null, user);
+				})
+				.catch(done);
+		}
+	)
+);
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const entryRoutes = require("./entry");
 const storyRoutes = require("./story");
+const userRoutes = require("./user");
 
 router.use("/entry", entryRoutes);
 router.use("/story", storyRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
